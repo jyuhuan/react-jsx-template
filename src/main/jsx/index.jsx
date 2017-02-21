1379616553393fb3c289f9037d43a1d30340ddd0
Yuhuan Jiang <jyuhuan@users.noreply.github.com>
2017-02-20 23:24:44 -0500

    Add files via upload

---
+++ src/main/jsx/index.jsx
@@ -0,0 +1,8 @@
+/**
+ * @author Yuhuan Jiang
+ */
+ReactDOM.render(
+  <div>The main JSX file is in src/main/jsx/index.jsx</div>, 
+  document.getElementById('root')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require("webpack")
+const path = require("path")
+
+const SrcMainDir = path.resolve(__dirname, "src/main/jsx")
+const TargetDir = path.resolve(__dirname, "target")
+
+
+module.exports = {
+  entry: path.join(SrcMainDir, "index.jsx"),
+  output: {
+    path: TargetDir,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test : /\.jsx?/,
+      include : SrcMainDir,
+      loader : 'babel-loader'
+    }]
+  }
+}
